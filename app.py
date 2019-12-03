19d506f3c9139c92223261a004fb21cd41565cff
Jordy Silva <19826749+jordy-silva@users.noreply.github.com>
2019-12-03 21:26:22 +0000

    Initial Commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template("base.html")
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
