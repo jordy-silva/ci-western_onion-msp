7401c4d1a519051dd75c049fa8eb06262f9e4494
Jordy Silva <19826749+jordy-silva@users.noreply.github.com>
2019-12-03 23:37:48 +0000

    Add new_request page

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def main():
     return render_template("base.html")
 
+@app.route('/new_request')
+def new_request():
+    return render_template("new_request.html")
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
