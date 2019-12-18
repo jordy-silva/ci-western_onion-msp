70f98e3864560575b2a9a638500ba130e2cfa8f9
Jordy Silva <19826749+jordy-silva@users.noreply.github.com>
2019-12-18 20:11:51 +0000

    document code and cleanup

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ def new_request():
 def add_payout(customer_id):
     return render_template("payout.html", action="add", customer=mongo.db.customers.find_one({"_id": ObjectId(customer_id)}))
 
+
 @app.route('/edit_payout/<customer_id>/<payout_id>')
 def edit_payout(customer_id, payout_id):
     return render_template("payout.html", action="edit", customer=mongo.db.customers.find_one({"_id": ObjectId(customer_id)}), payout=mongo.db.payouts.find_one({"_id": ObjectId(payout_id)}))
@@ -52,6 +53,7 @@ def create_records():
     mongo.db.payouts.insert_one(payout)
     return redirect(url_for('show_all'))
 
+
 @app.route('/update_records', methods=['POST'])
 def update_records():
     customer = {}  # Create a new empty dictionary for customer data
@@ -60,17 +62,24 @@ def update_records():
         # If data is about customer we add it to the customer dict
         if key == "recipient_name" or key == "country":
             customer[key] = value
+        # Otherwise it is added to the payout dict    
         else:
+            # Unless it's the _id's which will be used to define the document to oupdate
             if key != "customer_id" and key != "payout_id":
                 payout[key] = value
-    # If data doesn't contain the customer_id it means customer it's not in the database and we need to insert it
-    mongo.db.customers.update_one({"_id": ObjectId(request.form['customer_id'])}, {"$set": customer})
-    mongo.db.payouts.update_one({"_id": ObjectId(request.form['payout_id'])}, {"$set": payout})
+    # Update both customer and payout document
+    mongo.db.customers.update_one(
+        {"_id": ObjectId(request.form['customer_id'])}, {"$set": customer})
+    mongo.db.payouts.update_one(
+        {"_id": ObjectId(request.form['payout_id'])}, {"$set": payout})
     return redirect(url_for('show_all'))
+
 
 @app.route('/delete_payout/<customer_id>/<payout_id>')
 def delete_payout(customer_id, payout_id):
+    # Deletion of the payout
     mongo.db.payouts.remove({'_id': ObjectId(payout_id)})
+    # If there are no more payouts related to that customer, also delete the customer
     if not mongo.db.payouts.find_one({"customer_id": ObjectId(customer_id)}):
         mongo.db.customers.remove({'_id': ObjectId(customer_id)})
     return redirect(url_for('show_all'))
