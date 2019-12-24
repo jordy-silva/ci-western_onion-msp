46fa402e3a8cd1f8c5b38ef76ac0991b15fb8294
Jordy Silva <19826749+jordy-silva@users.noreply.github.com>
2019-12-24 02:11:43 +0100

    Update debug to False

--- app.py
+++ app.py
@@ -141,4 +141,4 @@ def delete_all():
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=False)
